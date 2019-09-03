b171fe3d34bbfb08f678ed76f4d36461ceafbf79
RSPOERRI <54564321+RSPOERRI@users.noreply.github.com>
2019-09-02 22:32:49 -0400

    TuringTarpitCompilerRun.rb
    very simple, but works

---
+++ TuringTarpitCompiler.rb
@@ -0,0 +1,179 @@
+=begin
+	=> Do not copy
+	=> @ Raphael Spoerri
+	=> Let's see it in action:
+=end
+
+def compile (code, show = false)
+
+#Make sure the front end doesn't choke on commas
+  	reg = /\S\,/
+	while reg.match(code)
+	
+		before = reg.match(code)[0]
+		after = before.sub(/,/, " ,")
+		code = code.sub(before, after)
+		
+	end
+#Make sure the front end doesn't choke on increment
+	reg = /\S\+\+/
+	while reg.match(code)
+	
+		before = reg.match(code)[0]
+		after = before.sub(/\+\+/, " ++")
+		code = code.sub(before, after)
+		
+	end
+#Make sure the front end doesn't choke on decrement
+	reg = /\S\-\-/
+	while reg.match(code)
+	
+		before = reg.match(code)[0]
+		after = before.sub(/\-\-/, " --")
+		code = code.sub(before, after)
+		
+	end
+	
+#divide the input into lines:
+	code = code.split("\n")
+	lexer = []
+#then into tokens
+	code.each do |line|
+		lexer << line.split(/\s/)
+	end
+#just _one_ predefined function, besides for print; user input:
+	out = "def ask\n\treturn gets.to_f\nend\n"
+	#iterate
+	for line in lexer
+		for lex in line
+			case lex
+				# Is equal to?
+				when "=?"
+					out += "== "
+					
+				when "="
+					out += " ="
+					
+				#the absence of value, like null
+				when "Mal"
+					out += "nil "
+					
+				when "If"
+					out += "if "
+					
+				when "Func"
+					out += "def "
+					
+				when "++"
+					out += " + 1"
+					
+				when "--"
+					out += " - 1"
+					
+				when "End"
+					out += "end"
+					
+				when "Return"
+					out += "return "
+					
+				when "Print"
+					out += "print "
+					
+				#comment
+				when "?:"
+					out += "#"
+					
+				else
+					unless /[^\w\(\)\>\<,\-\=\.]/.match(lex)
+						case lex.sub(/\(.*\)/, "").strip
+							#try to weed out other predefined functions and keywords
+							when "puts", "gets", "else", ".to_i", "and", "or", "not", "while", "else", "for", "elsif", "nil"
+								#DO ABSOLUTELY NOTHING
+							else
+							out += lex + " "
+						end
+					end
+			end
+		end
+		out += "\n"
+	end
+	if show
+		puts out + "\n\n"
+	end
+	eval out
+end
+
+
+
+#our code...
+x = >>CODE
+
+?: logical op `&`
+Func AND a, b
+	If a 
+		If b
+			Return 1
+		End
+	End
+	Return Mal
+End
+
+?: logical op `|`
+Func OR a, b
+	If a 
+		Return 1
+	End
+	If b
+		Return 1
+	End
+	Return Mal
+End
+Func pos i, x = 0
+	If i < 0
+		Return pos i++, x++
+	End
+	Return x
+End
+Func add a, b
+	If a < 1
+		Return b
+	End
+	Return add a--, b++
+End
+Func sub a, b
+	If b > 0
+		Return sub a--, b--
+	End
+	Return a
+End
+Func mul a, b, c = Mal
+	If AND a < 0, b < 0
+		a = pos a
+		b = pos b
+	End
+	If c =? Mal
+		c = b
+		b = 0
+	End
+	If a > 0
+		Return mul a--, add(b, c), c
+	End
+	Return b
+End
+Func flrdiv a, b, count = 0
+	If AND a =? 0, b =? 0
+		Return 00000000000000000000000000000000000000000000
+	End
+	If sub(a, b) >= 0
+		Return div sub(a, b), b, count ++
+	End
+	Return count
+End
+CODE
+
+#end code section
+
+#now Run!
+compl x, 5
+
+
